430cf0faf7c2e9e3d7221f91f2ff8876ae967f2c
Rodrigo Gonçalves <rodrigo@estudos.keitarobr.no-ip.org>
2024-01-22 20:11:12 +0000

    First build with PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api/menuitem';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenubarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'rgfinance-frontend';
+  menuOptions: MenuItem[] = [
+    {
+      label: 'Configuration',
+      icon: 'pi pi-fw pi-table',
+      items: [
+        {
+          label: 'Categories',
+          icon: 'pi pi-fw pi-table',
+          command: () => {
+            alert('Categories');
+          }
+        }
+      ]
+    }
+  ];
 }
